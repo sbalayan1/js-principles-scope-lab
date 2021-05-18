40de4f489e9cdff5e79664e78a13c40e7bba8ea7
sbalayan1 <balayans2014@yahoo.com>
2021-05-18 16:43:42 -0700

    commit tree 0795395afc494c8542fffdb3362e3763643e986a

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+
+//to change a variable from within a function, invoke the variable within the function and assign
+//example below
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = "hello world";
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = "You can't change me mom";
+}
